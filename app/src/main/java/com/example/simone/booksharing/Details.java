4e19cbf37b3c2643551a41ea3737feee4f5a85ff
Simosound94 <simonemerello@hotmail.it>
2016-04-14 15:32:03 +0200

    online

---
+++ app/src/main/java/com/example/simone/booksharing/Details.java
@@ -0,0 +1,14 @@
+package com.example.simone.booksharing;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Details extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
---
+++ app/src/main/java/com/example/simone/booksharing/Index.java
@@ -0,0 +1,18 @@
+package com.example.simone.booksharing;
+
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
+
+public class Index extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+   }
+
+}
