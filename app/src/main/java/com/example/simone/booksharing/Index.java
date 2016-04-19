07ea870ee8040cec899127bf63b9f60c6f677444
Simosound94 <simonemerello@hotmail.it>
2016-04-19 09:21:56 +0200

    created insert book fragment fixed some stuff

--- app/src/main/java/com/example/simone/booksharing/Index.java
+++ app/src/main/java/com/example/simone/booksharing/Index.java
@@ -2,6 +2,7 @@ package com.example.simone.booksharing;
 
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class Index extends Activity {
@@ -10,6 +11,10 @@ public class Index extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+        if(getResources().getConfiguration().orientation== Configuration.ORIENTATION_PORTRAIT)
+            return inflater.inflate(R.layout.fragment_index_port, container, false);
+        else
+            return inflater.inflate(R.layout.fragment_index_land, container, false);
    }
 
 }
