062c33be6a1b6886d9681496a212936a42ca49b5
Simosound94 <simonemerello@hotmail.it>
2016-04-19 21:16:15 +0200

    trying to inflate fragment

--- app/src/main/java/com/example/simone/booksharing/Index.java
+++ app/src/main/java/com/example/simone/booksharing/Index.java
@@ -5,16 +5,18 @@ import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
 
+
 public class Index extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+        /*
         if(getResources().getConfiguration().orientation== Configuration.ORIENTATION_PORTRAIT)
-            return inflater.inflate(R.layout.fragment_index_port, container, false);
+            getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingFragment()).commit();
         else
-            return inflater.inflate(R.layout.fragment_index_land, container, false);
+        */
    }
 
 }
