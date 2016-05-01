b6925bfe0a5a8dfcdcec88483288870d3660585a
AndreaPicasso <andrea0pica@gmail.com>
2016-05-01 14:38:59 +0200

    sdf

--- app/src/main/java/com/example/simone/booksharing/HomeFragment.java
+++ app/src/main/java/com/example/simone/booksharing/HomeFragment.java
@@ -8,11 +8,20 @@ import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import org.lucasr.twowayview.TwoWayView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class HomeFragment extends android.app.Fragment {
     public Button cerca;
+    public TwoWayView slider;
+
     public void onCreate(Bundle savedInstanceState) {
 
 
@@ -23,6 +32,10 @@ public class HomeFragment extends android.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_home,container,false);
         cerca=(Button) view.findViewById(R.id.cerca_button);
+        slider=(TwoWayView) view.findViewById(R.id.slider_lw);
+        ArrayList<HashMap<String,String>> items= new ArrayList<>();
+       
+
         View.OnClickListener cerca_l= new View.OnClickListener() {
             @Override
             public void onClick(View v) {
