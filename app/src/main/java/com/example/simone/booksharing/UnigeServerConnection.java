cbb83b76ba0e3589ca9bf78dbc9e29962efcf737
Simosound94 <simonemerello@hotmail.it>
2016-05-03 09:42:38 +0200

    Registration done.

---
+++ app/src/main/java/com/example/simone/booksharing/UnigeServerConnection.java
@@ -0,0 +1,15 @@
+package com.example.simone.booksharing;
+
+
+public class UnigeServerConnection {
+    public static final String URL = "http://webdev.dibris.unige.it/~S3940125/ANDROID_ENGINE/";
+
+    public static final String LOGIN = "query_login.php";
+    public static final String REGISTRATION = "query_registration.php";
+    public static final String PSW_DIMENTICATA = "query_psw_dimenticata.php";
+
+
+
+
+
+}
